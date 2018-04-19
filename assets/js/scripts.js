56999a68c8803a05a7a3af11647b077c42c9c0da
leroi <leroidejesa@gmail.com>
2018-04-19 12:19:06 -0700

    update 'bio' collection preview

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,4 +10,24 @@ $( document ).ready(function() {
       }
   );
 
+  $('.jubel-link-hover').hover(
+      function() {
+        $('.tagline-wrap').fadeOut(300);
+        $('.collection-jubel div').fadeIn(300);
+      },function() {
+        $('.collection-jubel div').fadeOut(300);
+        $('.tagline-wrap').fadeIn(300);
+      }
+  );
+
+  $('.bio-link-hover').hover(
+      function() {
+        $('.tagline-wrap').fadeOut(300);
+        $('.collection-bio .hidden').fadeIn(300);
+      },function() {
+        $('.collection-bio .hidden').fadeOut(300);
+        $('.tagline-wrap').fadeIn(300);
+      }
+  );
+
 });
