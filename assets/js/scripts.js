9072adc9c90c32c7b9fbb3c2d7ba9b8f29442acf
leroi <leroidejesa@gmail.com>
2018-04-19 13:14:04 -0700

    change tagline font, add bg mousemove/text mask effect

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
 
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    console.log(traX);
+    $(".tagline-wrap").css({"background-position": traX + "%" + traY + "%"});
+  });
+  // credit to @dghez_ for this mousemove effect
+
   $('.epicodus-link-hover').hover(
       function() {
         $('.tagline-wrap').fadeOut(300);
